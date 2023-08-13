03d63335b03338bfa9334f4104a9935a0dd54959
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-08-12 20:43:09 -0400

    Time: 47 ms (53.95%) | Memory: 16.2 MB (92.86%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,26 +1,13 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        num = 0 
-        isNeg = False 
-        if x < 0:
-            isNeg = True
-            x = x*-1
-        multi = 1 
-        y = x
-        while 10 <= y:
-            multi *= 10 
-            y = y//10
 
-        while 10 <= x:
-            val = x % 10
-            num += (val * multi)
-            x = x - val
-            x//=10
-            multi //= 10
-        num += x * multi
-        if num > abs(2147483647):
-            return 0
-        return num if not isNeg else -1 * num 
+        mxStr =  '2147483647'
 
+        ansLst = reversed(str(abs(x)))
+      
+        ansStr = ''.join(ansLst).rjust(10,'0')
 
+        if ansStr > mxStr: return 0
+       
+        return int(ansStr)*(1 - 2*(x < 0))
             
