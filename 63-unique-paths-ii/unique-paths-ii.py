dda539b28d3f0578979ae43a17662d4ac7829b43
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-08-18 11:24:08 -0400

    Time: 565 ms (5.95%) | Memory: 16.9 MB (7.12%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -5,7 +5,7 @@ class Solution:
 
         def dfs(x, y):
             print(x,y)
-            if x < 0 or y < 0 or x >= len(obstacleGrid) or y >= len(obstacleGrid[0]) or obstacleGrid[x][y]:
+            if x >= len(obstacleGrid) or y >= len(obstacleGrid[0]) or obstacleGrid[x][y]:
                 return 0
             if dp[x][y]:
                 return dp[x][y]
