a0b57cbad7e32b7c395f8ebfc846447c3d2bbfeb
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-08-20 22:28:50 -0400

    Time: 93 ms (95.81%) | Memory: 17.9 MB (75.08%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,25 +1,39 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = [[] for i in range(numCourses)] 
-        visitSet = set()
+        adj = defaultdict(list)
+        inDegrees = [0]*numCourses
 
-        for pair in prerequisites:
-            adj[pair[1]].append(pair[0])
-        
-        def dfs(i):
-            if i in visitSet:
-                return False
-            if len(adj[i]) == 0:
-                return True
-            visitSet.add(i)
-            for pre in adj[i]:
-                if not dfs(pre): return False
-            visitSet.remove(i)
-            adj[i] = [] 
-            return True
-        for course in range(1, numCourses):
-            if not dfs(course): return False
-        return True
+        for c,p in prerequisites:
+            adj[p].append(c)
+            inDegrees[c]+=1
+
+        q = deque() 
+        ans = []
+        visited = {i:False for i in range(numCourses)} 
+
+        for i in range(len(inDegrees)):
+            if inDegrees[i] == 0:
+                q.append(i)
+        while q:
+            current = q.popleft()
+            ans.append(current)
+
+
+            for next_course in adj[current]:
+                inDegrees[next_course] -= 1
+                if inDegrees[next_course] == 0:
+                    q.append(next_course)
+
+        return len(ans) == numCourses
+
+
+
+
+
+
+            
+
+
 
 
 
