4841e461770bc7166895e90991db212211a80d5c
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-08-21 10:15:13 -0400

    Time: 44 ms (86.02%) | Memory: 16.5 MB (16.35%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/repeated-substring-pattern.py
@@ -1,19 +1,4 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        count = 1 
-        while count < len(s):
-            if s[0:count] == s[count:count+count]:
-                run = 0
-                while run <= len(s):
-                    currWord = s[run:run+count]
-                    nextWord = s[run+count:run+count+count]
-                    if currWord == nextWord:
-                        if run+count+count == len(s):
-                            return True
-                    else:
-                        break
-                    run = run+count
-            count += 1
-        return False
-
+        return s in (s + s)[1:-1]
 
