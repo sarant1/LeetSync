af9b0cf33a418248f51c1d39518c2e759a06f596
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-08-31 12:34:19 -0400

    Time: 137 ms (70.24%) | Memory: 16.8 MB (46.90%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -1,19 +1,19 @@
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
-        min_pos = 0
-        max_pos = 0
-        count = 0  # Initialize variables to keep track of the minimum and maximum reach, and the number of taps used.
+        arr = [0] * (n + 1)
+        for i, r in enumerate(ranges):
+            if r == 0:
+                continue
+            left = max(0, i - r)
+            arr[left] = max(arr[left], i + r)
+
+        end, far_can_reach, cnt = 0, 0, 0
         
-        while max_pos < n:  # Continue looping until we cover the entire range from 0 to n.
-            for i in range(len(ranges)):
-                # Check if tap i can cover the current minimum position (min_pos) and reach the current maximum position (max_pos).
-                if i - ranges[i] <= min_pos and i + ranges[i] >= max_pos:
-                    max_pos = i + ranges[i]  # Update the maximum reach if this tap can cover the current range.
-                    
-            if max_pos == min_pos:
-                return -1  # If max_pos is not updated in this loop, it means we can't extend the reach further. Return -1 as it's not possible.
-            
-            count += 1  # Increment the count of taps used.
-            min_pos = max_pos  # Update the minimum reach to the newly reached maximum position.
-            
-        return count  # Return the count of taps used to cover the entire range.
+        for i, reach in enumerate(arr):
+            if i > end:
+                if far_can_reach <= end:
+                    return -1
+                end, cnt = far_can_reach, cnt + 1
+            far_can_reach = max(far_can_reach, reach)
+
+        return cnt + (end < n)
