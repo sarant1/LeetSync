71f02f02956300251fee4ae128a117ff7524d13e
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-09-24 20:14:51 -0400

    Time: 41 ms (60.95%) | Memory: 16.4 MB (43.09%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -2,8 +2,6 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         s = sorted(s)
         t = sorted(t)
-        print(s)
-        print(t)
         for i in range(len(t)-1): 
             if s[i] != t[i]:
                 return t[i]
