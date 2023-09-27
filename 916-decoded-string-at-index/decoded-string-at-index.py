c62f42aec70cca28c64123b7aa16c255b9490091
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-09-26 23:41:31 -0400

    Time: 33 ms (86.36%) | Memory: 15.9 MB (100.00%) - LeetSync

--- 916-decoded-string-at-index/decoded-string-at-index.py
+++ 916-decoded-string-at-index/decoded-string-at-index.py
@@ -8,14 +8,12 @@ class Solution:
             else:
                 length += 1
             i+=1
-        print(length)
         for j in range(i-1, -1, -1):
             char = s[j]
             # if it is a digit we are going to divide the length by the amount
             if char.isdigit():
                 length //= int(char)
                 k %= length
-                print("k: ", k)
             else:
                 if k == 0 or k == length:
                     return char
