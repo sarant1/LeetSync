c25b0d927fa21530ead8b9a44f73b32646141417
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-10-04 21:19:16 -0400

    Time: 117 ms (28.55%) | Memory: 17.5 MB (99.79%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,20 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        curr = 1
-        toPush = len(nums)/3
-        if toPush < 1:
-            return list(set(nums))
-        ans = []
-        for i in range(1, len(nums)):
-            if nums[i-1] != nums[i]:
-                curr = 1 
-            else:
-                curr += 1
-            if curr > toPush:
-                ans.append(nums[i])
-                curr = float("-inf")
-        return ans
+        return [num for num, count in Counter(nums).items() if count > len(nums)//3]
             
             
             
