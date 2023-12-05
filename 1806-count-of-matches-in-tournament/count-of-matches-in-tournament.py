b99f9c50f7181c46259ed87f488bb3f73efc9017
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-12-04 19:23:12 -0500

    Time: 45 ms (9.48%) | Memory: 16.4 MB (5.91%) - LeetSync

---
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        ans = 0
+        def dfs(t):
+            nonlocal ans
+            print(t)
+            if t <= 1:
+                return
+            if t % 2 == 0:
+                matches = t // 2
+                ans += matches
+                dfs(matches)
+            else:
+                matches = (t-1) // 2
+                ans += matches 
+                dfs(matches+1)
+        dfs(n)
+        return ans
+        
+
+
+        
