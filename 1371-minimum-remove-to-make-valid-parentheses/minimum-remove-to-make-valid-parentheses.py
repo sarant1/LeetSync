f806fda15223e86966086615d5ab8d90fff7b4ee
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-12-07 15:40:48 -0500

    Time: 76 ms (86.15%) | Memory: 18.3 MB (36.88%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,20 +1,20 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        stack = []
-        ans = list(s)
+      stack = []
+      s = list(s)
 
-        for i, char in enumerate(ans):
-            if char == "(":
+      for i, char in enumerate(s):
+          if char == "(":
+              stack.append(i)
+          elif char == ")":
+            if stack and s[stack[-1]] == "(":
+                stack.pop()
+            else:
                 stack.append(i)
-            elif char == ")":
-                if stack and s[stack[-1]] == "(":
-                    stack.pop()
-                else:
-                    stack.append(i)
-        for i in range(len(stack)):
-            ans[stack[i]] = ""
-        
-        return "".join(ans)
+      while stack:
+          s[stack.pop()] = ""
+      return "".join(s)
+      
 
       
     
