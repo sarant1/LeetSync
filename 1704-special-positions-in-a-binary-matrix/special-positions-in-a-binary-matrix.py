e64a5c907d560c96ed9123693c1cd1eea296f64b
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-12-12 22:58:28 -0500

    Time: 150 ms (77.37%) | Memory: 16.7 MB (64.83%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -1,22 +1,15 @@
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
-        ans = 0
-        cols = [0] * len(mat[0]) 
-        for i in range(len(mat)):
-            cur_sum = sum(mat[i])
-            if cur_sum == 1:
-                for j in range(len(mat[i])):
-                    if mat[i][j] and not cols[j]:
-                        ans+=1
-                        cols[j]+=1
-                    elif mat[i][j] and cols[j] == 1:
-                        ans-=1
-                        cols[j]+=1
-            elif cur_sum >= 2:
-                for j in range(len(mat[i])):
-                    if mat[i][j]:
-                        cols[j]+=2
-        return cols.count(1) 
+        def get_column_sum(col_idx):
+            return sum(row[col_idx] for row in mat)
+
+        special = 0
+        for row in mat:
+            if sum(row) == 1:
+                col_idx = row.index(1)
+                special += get_column_sum(col_idx) == 1
+
+        return special 
 
 
 
