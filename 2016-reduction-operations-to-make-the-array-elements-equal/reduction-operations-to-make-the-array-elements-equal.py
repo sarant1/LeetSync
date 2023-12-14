b76cb11e4c5d38dc41a366bd46e3ecefdd13a1bc
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-12-13 19:34:37 -0500

    Time: 854 ms (43.69%) | Memory: 24.1 MB (41.02%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
@@ -3,11 +3,11 @@ class Solution:
         nums.sort()
         n = len(nums)
         ans = 0
-
         for i in range(n-1):
             if nums[i] < nums[i+1]:
                 ans += n - i - 1
         return ans
+
 
 
 
