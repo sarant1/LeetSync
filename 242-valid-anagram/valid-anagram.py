1503b1413b476cbbcc0a2c2d9c4f2eff1e72b5d9
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-12-15 19:24:16 -0500

    Time: 62 ms (19.13%) | Memory: 16.8 MB (48.69%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,15 +2,14 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        letterMapS = {i:0 for i in range(26)}
-        letterMapT = {i:0 for i in range(26)} 
-
+        chars = [0] * 26
         for i in range(len(s)):
-            letterMapS[ord(s[i])-97]+=1
-            letterMapT[ord(t[i])-97]+=1 
-        if letterMapS == letterMapT:
-            return True
-        else:
-            return False
+            chars[ord(s[i])-ord("a")] += 1
+            chars[ord(t[i])-ord("a")] -= 1
+        for i in range(len(chars)):
+            if chars[i] != 0:
+                return False
+        return True
 
 
+        
