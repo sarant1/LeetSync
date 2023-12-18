45689863b37f7cb62de127626af9b9f828180ce1
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-12-18 14:05:39 -0500

    Time: 69 ms (55.91%) | Memory: 17.7 MB (32.71%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,20 +1,20 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        ans = 0
-        cache = {}
+        dp = [[-1 for _ in range(len(triangle[i]))] for i in range(len(triangle))]
         def dfs(i, j, res):
-            if i == len(triangle):
-                return res 
-            if (i, j) in cache:
-                return cache[(i, j)]
+            if i > len(triangle)-1:
+                return res
+            if dp[i][j] != -1:
+                return dp[i][j] 
             first = triangle[i][j] + dfs(i+1, j, res)
             second = triangle[i][j] + dfs(i+1, j+1, res)
             res = min(first, second)
-            
-            cache[(i, j)] = res
+            dp[i][j] = res
 
-            return cache[(i, j)]  
+            return dp[i][j]
         return dfs(0, 0, 0)
+
+
 
             
         
