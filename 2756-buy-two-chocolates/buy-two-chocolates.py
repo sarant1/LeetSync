466a195cc04e598432fc1c8bd9a421994017a852
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-12-19 19:48:48 -0500

    Time: 80 ms (5.42%) | Memory: 16.5 MB (23.41%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -1,13 +1,5 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        m = money
-        f, s = float('inf'), float('inf')
-        for num in prices:
-            if num < f and num < s:
-                s, f = f, num 
-            elif num < s:
-                s = num
-        m-=f 
-        m-=s
-        return m if m >= 0 else money
+        total = sum(heapq.nsmallest(2, prices))
+        return money-total if money-total >= 0 else money
         
