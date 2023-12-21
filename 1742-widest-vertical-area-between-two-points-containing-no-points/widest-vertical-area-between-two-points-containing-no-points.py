075db3ef00457599c843edbb14e1ac5ff942524d
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-12-20 19:14:29 -0500

    Time: 739 ms (75.31%) | Memory: 60.3 MB (5.86%) - LeetSync

--- 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.py
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.py
@@ -3,7 +3,6 @@ class Solution:
         points.sort()
         ans = 0
         for i in range(1, len(points)):
-            if points[i-1] != points[i]:
-                ans = max(ans, points[i][0]-points[i-1][0])
+            ans = max(ans, points[i][0]-points[i-1][0])
         return ans
         
