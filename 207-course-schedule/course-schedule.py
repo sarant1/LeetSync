3449bc53b09f54fa434160f2bd9fcf2e6cd516cb
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-12-24 10:52:38 -0500

    Time: 94 ms (81.12%) | Memory: 19 MB (58.36%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,21 +1,21 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj = [[] for _ in range(numCourses)]
-        inDegree = [0] * numCourses
+        inDegrees = [0] * numCourses
         for course in prerequisites:
             adj[course[1]].append(course[0])
-            inDegree[course[0]]+=1
+            inDegrees[course[0]]+=1
         stack = []
         for i in range(numCourses):
-            if inDegree[i] == 0:
+            if inDegrees[i] == 0:
                 stack.append(i)
         while stack:
             cur = stack.pop()
             for course in adj[cur]:
-                inDegree[course]-=1
-                if inDegree[course] == 0:
+                inDegrees[course]-=1
+                if inDegrees[course] == 0:
                     stack.append(course)
-        return all(inD==0 for inD in inDegree)
+        return all(inDegree == 0 for inDegree in inDegrees)
         
 
         
