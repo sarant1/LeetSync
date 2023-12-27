1f0bcab9996cdf8bb086d5f56cab2c456c9199c6
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2023-12-27 13:46:41 -0500

    Time: 439 ms (40.26%) | Memory: 17.2 MB (57.33%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
@@ -1,15 +1,14 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        dp = [0] * (target + 1)
+        mod = 10**9+7
+        dp = [0] * (target+1)
         dp[0] = 1
-        MOD = 10**9+7
 
         for dice in range(n):
             next_dp = [0] * (target+1)
-
-            for val in range(1, k+1):
-                for total in range(val, target+1):
-                    next_dp[total] = (next_dp[total] + dp[total-val]) % MOD
+            for dice_amt in range(1, k+1):
+                for total in range(dice_amt, target+1):
+                    next_dp[total] = (next_dp[total] + dp[total-dice_amt]) % mod 
             dp = next_dp
         return dp[target]
 
