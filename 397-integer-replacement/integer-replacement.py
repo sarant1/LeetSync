c6230411e8ee41f7ed3135ada62484feba0b68e6
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2024-01-01 13:31:01 -0500

    Time: 181 ms (16.84%) | Memory: 17.1 MB (13.55%) - LeetSync

--- 397-integer-replacement/integer-replacement.py
+++ 397-integer-replacement/integer-replacement.py
@@ -1,12 +1,9 @@
 class Solution:
     def integerReplacement(self, n: int) -> int:
         ans = 0
-        cache = {}
         def count(total, res):
             if total == 1:
                 return res
-            if (total, res) in cache:
-                return cache[(total, res)]
             if total % 2:
                 res = min(
                         count(total+1, res+1),
@@ -14,7 +11,6 @@ class Solution:
                         )
             else:
                 res = count(total//2, res+1)
-            cache[(total, res)] = res
             return res
         return count(n, 0)
             
