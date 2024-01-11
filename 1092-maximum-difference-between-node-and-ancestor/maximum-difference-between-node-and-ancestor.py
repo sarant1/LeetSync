2ac083eda0aa35934715f50afb8720ea8c0f7dbc
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2024-01-11 17:15:08 -0500

    Time: 49 ms (33.33%) | Memory: 19.1 MB (73.40%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
@@ -6,20 +6,22 @@
 #         self.right = right
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-        maxVal = 0
+        def dfs(node, maxVal, minVal, ans):
+            if not node:
+                return 0
+            maxVal = max(node.val, maxVal)
+            minVal = min(node.val, minVal)
+            diff1 = max(ans, dfs(node.left,maxVal, minVal, ans))
+            diff2 = max(ans, dfs(node.right,maxVal, minVal, ans))
+            currdiff = max(diff1, diff2)
+            return max(ans, node.val-minVal, maxVal-node.val, currdiff)
+        return dfs(root, root.val, root.val, 0)
+            
+            
+        
+        
+            
 
-        queue = deque([(root, root.val, root.val)])
-        while queue:
-            curr, high, low = queue.popleft()
-            currVal = curr.val
-            diff1, diff2 = currVal - low, high - currVal
-            maxVal = max(maxVal, diff1, diff2)
-
-            if curr.left:
-                queue.append((curr.left, max(currVal, high), min(currVal, low)))
-            if curr.right:
-                queue.append((curr.right, max(currVal, high), min(currVal, low)))
-        return maxVal
 
 
         
