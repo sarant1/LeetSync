906ea1e4b398290b0de6a5e36beb716ca0f4779d
Samuel Arant <110504235+sarant1@users.noreply.github.com>
2024-02-21 20:59:26 -0500

    Time: 603 ms (64.54%) | Memory: 21.5 MB (95.97%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        if n == 1 and not trust:
+            return 1
+        elif not trust:
+            return -1
+        trusting = [0] * (n + 1)
+        checked = set() 
+        judge = -1
+        for boy in trust:
+            trusting[boy[1]]+=1
+            checked.add(boy[0])
+        cur = -1
+        for i in range(len(trusting)):
+            if trusting[i] > cur:
+                cur = trusting[i]
+                judge = i
+        print(trusting)
+        print(judge)
+        return judge if judge not in checked and len(checked) + 1 == n else -1
+
+        
